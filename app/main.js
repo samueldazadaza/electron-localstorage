743f6ce1ef6302506ca494dc182d229a71c81799
sam <samueldazadaza@gmail.com>
2021-11-09 22:30:16 -0500

    interfaz basica creada

--- app/main.js
+++ app/main.js
@@ -1,6 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow = null //para que app siga funcionando
+
 app.on('ready', () => {
-    mainWindow = new BrowserWindow();
+    mainWindow = new BrowserWindow({
+        webPreferences : {
+            nodeIntegration: true //obtener api de node en ventana
+        }
+    });
     mainWindow.loadFile(__dirname + '/index.html')
 });
